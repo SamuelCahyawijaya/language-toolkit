8ceb4bb2ca9ec52ec59a5df9f861fbdffc2006df
Samuel Cahyawijaya <scahyawijaya@connect.hk.ust>
2023-09-15 17:18:14 +0800

    update download fn - add optional parameter name

--- iso639.py
+++ iso639.py
@@ -6,9 +6,13 @@ import matplotlib.pyplot as plt
 from unicodedata import normalize
 
 # Download Manager
-def download_iso639_3_map(out_dir='./'):
+def download_iso639_3_map(out_dir='./', out_name=None):
     url = f'https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3_Name_Index.tab'
-    out_path = f'{out_dir}/iso-639-3.tab'
+    if out_name is None:
+        out_path = f'{out_dir}/iso-639-3.tab'
+    else:
+        out_path = f'{out_dir}/{out_name}'
+        
     r = requests.get(url, allow_redirects=True, stream=True)
     with open(out_path, 'wb') as file:
         for chunk in tqdm(r.iter_content(chunk_size=1024)):
@@ -16,8 +20,12 @@ def download_iso639_3_map(out_dir='./'):
                 file.write(chunk)
     return out_path
 
-def download_wiki_iso_map(out_dir='./'):
-    out_path = f'{out_dir}/wiki_iso_mapping.csv'
+def download_wiki_iso_map(out_dir='./', out_name=None):
+    if out_name is None:
+        out_path = f'{out_dir}/wiki_iso_mapping.csv'
+    else:
+        out_path = f'{out_dir}/{out_name}'
+    
     wiki_iso_df = pd.read_html('https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes')[0]
     wiki_iso_df.columns = ['639-2', '639-3', '639-5', '639-1', 'lang_name', 'scope', 'type', 'native_name', 'other_name']
 
@@ -27,9 +35,13 @@ def download_wiki_iso_map(out_dir='./'):
     wiki_iso_df.to_csv(f'{out_dir}/wiki_iso_mapping.csv', index=False)
     return out_path
 
-def download_iso639_2_map(out_dir='./'):
+def download_iso639_2_map(out_dir='./', out_name=None):
     url = f'https://www.loc.gov/standards/iso639-2/php/code_list.php'
-    out_path = f'{out_dir}/iso-639-2.tab'    
+    if out_name is None:
+        out_path = f'{out_dir}/iso-639-2.tab' 
+    else:
+        out_path = f'{out_dir}/{out_name}'
+
     r = requests.get(url, allow_redirects=True, stream=True)
     with open(out_path, 'wb') as file:
         for chunk in tqdm(r.iter_content(chunk_size=1024)):
